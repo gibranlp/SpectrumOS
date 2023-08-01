9ec116515e075e780155a2aa2118447385176cbe
gibranlp <root@gibranlp.dev>
2023-08-01 11:26:56 -0600

    updated thumbnails for screenshots and wallpapers

---
+++ resize.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# _____             _                 _____ _____ 
+#|   __|___ ___ ___| |_ ___ _ _ _____|     |   __|
+#|__   | . | -_|  _|  _|  _| | |     |  |  |__   |
+#|_____|  _|___|___|_| |_| |___|_|_|_|_____|_____|
+#      |_|   
+# SpectrumOS - Embrace the Chromatic Symphony!
+# By: gibranlp <thisdoesnotwork@gibranlp.dev>
+# MIT licence 
+# 
+#
+
+#Resize all images to create thumbnails
+
+for filename in $1/*.png; do
+    convert $filename -resize 208x117 "$1/$(basename "$filename" .png)-sm.png"
+done
