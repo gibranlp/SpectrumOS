2d41c535fa128006c4d25836c1a5da9d59885911
gibranlp <root@gibranlp.dev>
2025-11-17 20:41:39 -0500

    Fix rofi.nix syntax error

--- home/hyprland/rofi.nix
+++ home/hyprland/rofi.nix
@@ -1,34 +1,16 @@
-programs.rofi = {
-  enable = true;
-  package = pkgs.rofi-wayland;
-  
-  extraConfig = {
-    modi = "drun,run,window";
-    show-icons = true;
-    terminal = "kitty";
-    drun-display-format = "{icon} {name}";
-    sidebar-mode = true;
-  };
-};
+{ config, pkgs, ... }:
 
-# Use a separate rasi file for theming
-home.file.".config/rofi/config.rasi".text = ''
-  @import "~/.cache/wal/colors-rofi.rasi"
-  
-  * {
-    bg-col: @background;
-    bg-col-light: @color0;
-    border-col: @color4;
-    selected-col: @color4;
-    fg-col: @foreground;
-    width: 600;
-  }
-  
-  window {
-    height: 360px;
-    border: 3px;
-    border-color: @border-col;
-    background-color: @bg-col;
-    border-radius: 10px;
-  }
-'';
+{
+  programs.rofi = {
+    enable = true;
+    package = pkgs.rofi-wayland;
+    
+    extraConfig = {
+      modi = "drun,run,window";
+      show-icons = true;
+      terminal = "kitty";
+      drun-display-format = "{icon} {name}";
+      sidebar-mode = true;
+    };
+  };
+}
